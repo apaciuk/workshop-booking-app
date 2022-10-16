bb4b2b42b8c4f14f40102bf7c8aca06b64f082a7
Paul Anthony McGowan <xhostcom@aol.com>
2022-10-16 19:11:07 +0100

    Modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,14 @@
 Admin::User.create(email: 'admin@example.com', password: 'Password9957', password_confirmation: 'Password9957')
 
 Workshop.create!(
-    name: 'Workshop 1', 
+    name: 'Workshop 1',
     description: 'This is a workshop', 
-    start_date: '2021-01-01', 
-    end_date: '2021-01-02', 
+    start_date: Date.today, 
+    end_date: Date.today + 1.day, 
     start_time: '10:00', 
     end_time: '11:00', 
     total_sits: 10, 
     remaining_sits: 5,
-    registration_fee: 0
+    registration_fee: 1500
 )
 
